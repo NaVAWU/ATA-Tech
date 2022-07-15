dddd0710f57d89576f27728e6e1031329781a816
Naim Verboom <naimverboom@gmail.com>
2022-07-15 23:13:33 +0200

    Burgir

--- src/main/java/nl/naimv/atatech/item/ModFoodComponents.java
+++ src/main/java/nl/naimv/atatech/item/ModFoodComponents.java
@@ -7,4 +7,5 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent MERICAN_CHEESE = new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 40, 1), 1.0f).build();
+    public static final FoodComponent MERICAN_BURGER = new FoodComponent.Builder().hunger(12).saturationModifier(0.8f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 2), 1.0f).build();
 }
--- src/main/java/nl/naimv/atatech/item/ModItems.java
+++ src/main/java/nl/naimv/atatech/item/ModItems.java
@@ -5,12 +5,15 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import nl.naimv.atatech.ATATech;
+import nl.naimv.atatech.item.custom.MericanBurgerItem;
 import nl.naimv.atatech.item.custom.MericanCheeseItem;
 
 public class ModItems {
     public static final Item MERICAN_CHEESE = registerItem("merican_cheese",
             new MericanCheeseItem(new FabricItemSettings().food(ModFoodComponents.MERICAN_CHEESE).group(ModItemGroup.ATATECH)));
 
+    public static final Item MERICAN_BURGER = registerItem("merican_burger",
+            new MericanBurgerItem(new FabricItemSettings().food(ModFoodComponents.MERICAN_BURGER).group(ModItemGroup.ATATECH)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ATATech.MOD_ID, name), item);
