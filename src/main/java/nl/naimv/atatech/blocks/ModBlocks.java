274acbcbb1a211c2d0d28c20e100d66f316d9a36
Thunder_Above <FryDThundor@gmail.com>
2022-07-16 17:17:19 -0400

    added Wormhole Frame

--- src/main/java/nl/naimv/atatech/blocks/ModBlocks.java
+++ src/main/java/nl/naimv/atatech/blocks/ModBlocks.java
@@ -1,17 +1,22 @@
 package nl.naimv.atatech.blocks;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import nl.naimv.atatech.ATATech;
+import nl.naimv.atatech.item.ModItemGroup;
 
 
 public class ModBlocks {
 
+    public static final Block WORMHOLE_FRAME = registerBlock("wormhole_frame",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(8f).requiresTool()), ModItemGroup.ATATECH);
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
         return Registry.register(Registry.BLOCK, new Identifier(ATATech.MOD_ID,name), block);
