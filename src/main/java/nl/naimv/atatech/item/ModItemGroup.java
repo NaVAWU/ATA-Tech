51624f6be19b5344be07e4df06506982f46bd21d
Thunder_Above <FryDThundor@gmail.com>
2022-07-19 15:48:10 -0400

    Changed ItemGroup logo to Wormhole Frame

--- src/main/java/nl/naimv/atatech/item/ModItemGroup.java
+++ src/main/java/nl/naimv/atatech/item/ModItemGroup.java
@@ -6,8 +6,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import nl.naimv.atatech.ATATech;
+import nl.naimv.atatech.block.ModBlocks;
 
 public class ModItemGroup {
     public static final ItemGroup ATATECH = FabricItemGroupBuilder.build(new Identifier(ATATech.MOD_ID, "atatech"),
-            () -> new ItemStack(ModItems.MERICAN_CHEESE));
+            () -> new ItemStack(ModBlocks.WORMHOLE_FRAME));
 }
